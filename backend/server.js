e2457d406a76c26e87ef50f6d0dfec50e43fe713
fredericoricco-debug <75469834+fredericoricco-debug@users.noreply.github.com>
2024-09-16 19:01:29 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -291,8 +291,8 @@ app.post('/api/submit-feedback', (req, res) => {
   const sgMail = require('@sendgrid/mail')
   sgMail.setApiKey(process.env.SENDGRID_API_KEY)
   const msg = {
-    to: 'test@example.com', // Change to your recipient
-    from: 'test@example.com', // Change to your verified sender
+    to: 'frederico@l4wb-i.org', // Change to your recipient
+    from: 'admin@l4wb-i.org', // Change to your verified sender
     subject: 'Sending with SendGrid is Fun',
     text: 'and easy to do anywhere, even with Node.js',
     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
